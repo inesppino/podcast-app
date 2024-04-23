b7e7883e005bf13db2739ec6bd7e35a1002179b1
inesppino <inesp.pino@gmail.com>
2024-04-23 19:59:11 +0200

    :sparkles: add spinner to header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,14 @@
+import { useSelector } from "react-redux";
+import { Spinner } from "../spinner";
+
 const Header = () => {
+  const isLoading = useSelector((state) => state.global.isLoading);
   return (
     <header className="header__container">
       <a href="/">
         <h1>Podcaster</h1>
       </a>
+      {isLoading ? <Spinner /> : null}
     </header>
   );
 };
