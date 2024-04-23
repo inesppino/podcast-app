c5980e388f48cc26c6b9c6ba31f9e16090071243
inesppino <inesp.pino@gmail.com>
2024-04-23 13:38:01 +0200

    :sparkles: build router in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./Home.jsx";
 import "./styles/globals.scss";
+import Podcast from "./Podcast.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "podcast/:podcastId",
+    element: <Podcast />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
